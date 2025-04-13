c8ddd63e5cf4d313829746f8838ba13f7f6672fd
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2025-04-13 14:57:09 -0500

    commit

---
+++ counterWatch Watch App/counterWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterWatchApp.swift
+//  counterWatch Watch App
+//
+//  Created by Daniel Sarmiento on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct counterWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watch Watch App/watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchApp.swift
+//  watch Watch App
+//
+//  Created by Daniel Sarmiento on 13/04/25.
+//
+
+import SwiftUI
+
+@main
+struct watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
