4255ddff610eb1fda7f139bbdbb17dc5a242b217
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2025-04-12 23:49:53 -0500

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Daniel Sarmiento on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
